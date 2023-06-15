0687ebcd5b33562f514b9203d89568ae8677112f
Quyet <huuquyetng@gmail.com>
2023-06-16 01:00:43 +0700

    Prepare Skirmish 5

---
+++ rpciege_5_deployer/src/lib.rs
@@ -0,0 +1,11 @@
+#![no_std]
+
+mod contract;
+mod storage;
+mod types;
+
+pub mod liqpool {
+    soroban_sdk::contractimport!(
+        file = "../../target/wasm32-unknown-unknown/release/rpciege_5_liquidity_pool.wasm"
+    );
+}
